755d03408fcd6a7e8429c6a7fe588141c78d4bbc
Avery Wagar <avery@wagar.cc>
2019-09-25 17:24:40 -0700

    Move to interval based ldm

--- svr/middleware/ld.js
+++ svr/middleware/ld.js
@@ -1,38 +1,54 @@
-//const express = require('express');
 var LaunchDarkly = require("launchdarkly-node-server-sdk");
+
+// TODO: Pass user through server.js or middleware
+const ldUser = {
+  firstName: "AS",
+  lastName: "DAF",
+  key: "asdasdas@example.com",
+  custom: {
+    groups: "beta_testers"
+  }
+};
+
+// These are global variables, kinda hacky
+// but it keeps server.js clean
+let ldflags = {};
+let ldclient;
+
+// Init LD middleware
+// Defaults to 60 second interval, and default user
+const init = async (ldkey, interval=60000, user=ldUser) => {
+  // Initialize LaunchDarkly
+    ldclient = LaunchDarkly.init(
+    ldkey
+  );
+
+  // After Initialization
+  ldclient.on("ready", async () => {
+    await updateFlags(user)
+  });
+
+  // Set an update interval
+  setTimeout(() => updateFlags(user), interval)
+}
+
+// Update flags from LaunchDarkly
+const updateFlags = async user => {
+  console.log("Updating flags");
+  let state = await ldclient.allFlagsState(user);
+  ldflags = state.allValues();
+}
 
 // request id middleware
 const ldm = flagName => {
   return (req, res, next) => {
-    let ldclient = LaunchDarkly.init(
-      "sdk-6203c9da-4d89-474d-9dc0-4c01345eb30b"
-    );
-
-    let user = {
-      firstName: "AS",
-      lastName: "DAF",
-      key: "asdasdas@example.com",
-      custom: {
-        groups: "beta_testers"
-      }
+    req.flags = {
+      ...req.flags,
+      [flagName]: ldflags[flagName]
     };
-
-    ldclient.once("ready", function() {
-      ldclient.variation(flagName, user, false, function(err, showFeature) {
-        req.flags = {
-          ...req.flags,
-          [flagName]: showFeature
-        };
-        console.log(req.flags);
-
-        ldclient.flush(function() {
-          ldclient.close();
-        });
-
-        next();
-      });
-    });
+    console.log(req.flags);
+    next();
   };
 };
 
-module.exports = ldm;
+module.exports = {ldm, init};
--- svr/server.js
+++ svr/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const ldm = require("./middleware/ld");
+const UserController = require("./routes/users/index");
+const {ldm, init} = require("./middleware/ld");
 
 const app = express();
 const port = 3001;
 
-const UserController = require("./routes/users/index");
+// 5 Minute updateInterval
+const updateInterval = 5 * 60000;
+// LaunchDarkly SDK key
+const ldKey= "sdk-6203c9da-4d89-474d-9dc0-4c01345eb30b";
+
+// Update LaunchDarkly Flags every 5 minutes
+init(ldKey, updateInterval);
 
 app.use(express.json());
 app.use(ldm("user-details"));
